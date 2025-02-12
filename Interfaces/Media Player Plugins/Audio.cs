2c925ef4353a38852b1184635e17db0869ff393b
rndody <rndody@gmail.com>
2025-02-12 21:59:21 +0200

    Media Player Plugins

---
+++ Interfaces/Media Player Plugins/Audio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Media_Player_Plugins
+{
+    public class Audio : IMedia
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing audio");
+            
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopping audio");
+            
+        }
+    }
+}
---
+++ Interfaces/Media Player Plugins/IMedia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Media_Player_Plugins
+{
+    internal interface IMedia
+    {
+        public void Play();
+        public void Stop();
+    }
+}

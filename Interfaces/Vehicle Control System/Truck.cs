3bd3a5aad48f3807f81cf9d0300b45d1ad1b00fa
rndody <rndody@gmail.com>
2025-02-12 21:48:26 +0200

    Vehicle Control System

---
+++ Interfaces/Vehicle Control System/Truck.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Vehicle_Control_System
+{
+    internal class Truck : Vehicle, ISwitchable
+    {
+        public string Name { set; get; }
+        public string Model { set; get; }
+        public int Year { set; get; }
+        public decimal Price { set; get; }
+
+        public override void Start()
+        {
+            Console.WriteLine("Start");
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine("Stop");
+
+        }
+
+        public void SwitchOff()
+        {
+            Console.WriteLine("turn off");
+        }
+
+        public void SwitchOn()
+        {
+            Console.WriteLine("turn on");
+        }
+    }
+}
---
+++ Interfaces/Vehicle Control System/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Vehicle_Control_System
+{
+    internal abstract class Vehicle
+    {
+        private  string name;
+        private string model;
+        private int year;
+        private decimal price;
+
+
+
+        public abstract void Start();
+        public abstract void Stop();
+    }
+}

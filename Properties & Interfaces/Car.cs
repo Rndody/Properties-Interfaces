99573b4db284bf982c47caa8ce74fac140deb85b
rndody <rndody@gmail.com>
2025-02-12 19:35:29 +0200

    Properties

---
+++ Properties & Interfaces/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties___Interfaces
+{
+    internal class Car
+    {
+        #region Properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+        public int Age
+        {
+            get
+            {
+                return DateTime.Now.Year - Year;
+            }
+        }
+        #endregion
+
+
+        #region Constructors
+        public Car(string make, string model, int year, int mileage)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Mileage = mileage;
+        }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Mileage = 0;
+        }
+        #endregion
+
+
+        #region Methods
+
+        public void Drive(int distance)
+        {
+            if (distance >= 0) 
+            Mileage += distance;
+        } 
+        #endregion
+
+
+
+
+    }
+}
--- Properties & Interfaces/Product.cs
+++ Properties & Interfaces/Product.cs
@@ -15,10 +15,57 @@ namespace Properties___Interfaces
         #endregion
 
 
+        #region Properties
         public string Name { get; set; }
-        public decimal Price { get; set; }
-        public int StockQuantity { get; set; }
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value >= 0)
+                    price = value;
+            }
+        }
+        public int StockQuantity
+        {
+            get
+            {
+                return stockQuantity;
+            }
+            set
+            {
+                if (value >= 0)
+                    stockQuantity = value;
+                else stockQuantity = 0;
+            }
+        }
         public bool IsAvailable { get; set; }
+        #endregion
+
+
+        #region Constructors
+        public Product(string name, decimal price, int stock)
+        {
+            Name = name;
+            Price = price;
+            if (stock >= 0)
+                StockQuantity = stock;
+            else stock = 0;
+        }
+        #endregion
+
+
+
+        #region Methods
+        public void UpdateStock(int stockAdded, int stockDeducted)
+        {
+            if (stockAdded - stockDeducted + StockQuantity >= 0)
+                StockQuantity = StockQuantity + stockAdded - stockDeducted;
+        }
+        #endregion
 
 
 
